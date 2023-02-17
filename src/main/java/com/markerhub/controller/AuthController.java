7b85dc512ee290199c8dc2a08874a7d6b9559889
ShaoHuang WU <shaohuang.wu1@se.com>
2023-02-17 13:09:14 +0800

    获取当前用户信息接口

--- src/main/java/com/markerhub/controller/AuthController.java
+++ src/main/java/com/markerhub/controller/AuthController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.map.MapUtil;
 import com.google.code.kaptcha.Producer;
 import com.markerhub.common.lang.Const;
 import com.markerhub.common.lang.Result;
+import com.markerhub.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.UUID;
 
 
@@ -42,4 +44,24 @@ public class AuthController extends BaseController {
                 MapUtil.builder().put("token",key).put("base64Img",base64Img).build()
         );
     }
+
+    /**
+     * 获取用户信息接口
+     * @param principal
+     * @return
+     */
+    @GetMapping("/sys/userInfo")
+    public Result userInfo(Principal principal) {
+
+        SysUser sysUser = sysUserService.getByUsername(principal.getName());
+
+        return Result.succ(MapUtil.builder()
+                .put("id", sysUser.getId())
+                .put("username", sysUser.getUsername())
+                .put("avatar", sysUser.getAvatar())
+                .put("created", sysUser.getCreated())
+                .map()
+        );
+    }
+
 }
